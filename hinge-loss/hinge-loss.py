9b36803006ddeb7220bd6479411e1c7a92e8c5cd
Thanh Hưng Phạm (Tommy) <124342674+ThanhHungtaptanhhocpython@users.noreply.github.com>
2026-04-27 15:32:42 +0700

    Add solution for hinge-loss

---
+++ hinge-loss/hinge-loss.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+def hinge_loss(y_true, y_score, margin=1.0, reduction="mean") -> float:
+    """
+    y_true: 1D array of {-1,+1}
+    y_score: 1D array of real scores, same shape as y_true
+    reduction: "mean" or "sum"
+    Return: float
+    """
+    # Write code here
+    y_true = np.array(y_true)
+    y_score = np.array(y_score)
+    losses = np.maximum(0, margin - (y_true*y_score))
+    if reduction == "mean":
+        return np.mean(losses)
+    elif reduction == "sum":
+        return np.sum(losses)
+    else:
+        raise ValueError("reduction must be 'mean' or 'sum'")
+    pass
