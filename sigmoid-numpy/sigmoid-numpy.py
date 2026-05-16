151027d5464e618e846aeea81b33632e2b5541f1
ThanhHungtaptanhhocpython <283856752+pthanhhung000-boop@users.noreply.github.com>
2026-05-16 09:26:27 +0700

    robust sigmoid

--- sigmoid-numpy/sigmoid-numpy.py
+++ sigmoid-numpy/sigmoid-numpy.py
@@ -5,7 +5,6 @@ def sigmoid(x):
     Vectorized sigmoid function.
     """
     # Write code here
-    x = np.array(x)
-    losses = 1/(1+np.exp(-x))
-    return losses 
-    pass
+    x = np.asrray(x, dtype=float)
+    results = np.where(x >=0, 1/(1+np.exp(-x)), np.exp(x)/(1 + np.exp(x)))
+    return results
