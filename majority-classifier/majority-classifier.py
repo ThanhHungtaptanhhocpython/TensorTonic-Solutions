b468b2901b948dafb2d7eeb42bdab8f56158a348
Thanh Hưng Phạm (Tom) <124342674+ThanhHungtaptanhhocpython@users.noreply.github.com>
2026-05-25 17:28:52 +0700

    Add solution for majority-classifier

---
+++ majority-classifier/majority-classifier.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+def majority_classifier(y_train, X_test):
+    """
+    Predict the most frequent label in training data for all test samples.
+    """
+    # Write code here
+    y_train = np.asarray(y_train)
+    X_test = np.asarray(X_test)
+
+
+    labels, counts = np.unique(y_train, return_counts=True)
+    
+    majority_label = labels[np.argmax(counts)]
+    
+    n_samples = X_test.shape[0] if X_test.ndim > 1 else len(X_test)
+    
+    predictions = np.full(n_samples, majority_label)
+
+    return predictions
